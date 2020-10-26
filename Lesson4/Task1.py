7093f0529945489553ee7d5fd89a0600423bd5f1
Stretop <stretop@gmail.com>
2020-10-26 22:30:52 +0300

    Quickfix

--- Lesson4/Task1.py
+++ Lesson4/Task1.py
@@ -3,10 +3,12 @@
 В расчете необходимо использовать формулу: (выработка в часах * ставка в час) + премия.
 Для выполнения расчета для конкретных значений необходимо запускать скрипт с параметрами.
 """
+from sys import argv
 
+scriptname, worktime, rate, bonus = argv
 
 def f_wage(worktime, rate, bonus):
     return (worktime * rate) + bonus
 
 
-print(f_wage(2, 3, 4))
+print(f_wage(float(worktime), float(rate), float(bonus)))
